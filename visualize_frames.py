26ca5fe27c578cf768569ad8847fb24dd6bb45c9
Andrea <andrea.klaus.tubak@gmail.com>
2021-10-21 17:16:13 +0200

    [WIP] consume frames sychronously
    resize_image needs to work all in memory

---
+++ visualize_frames.py
@@ -0,0 +1,51 @@
+# import the opencv library
+import cv2
+import time
+
+def rescale_frame(frame, width, height):
+	dimensions = (width, height)
+	
+	return cv2.resize(frame, dimensions, interpolation=cv2.INTER_AREA)
+
+
+video_path = 'Rec_20200207_142322_151_M.mp4'
+
+# define a video capture object
+cap = cv2.VideoCapture(video_path)
+
+fps = cap.get(cv2.CAP_PROP_FPS)
+amount_of_frames = cap.get(7)
+
+prev = 0
+
+while cap.isOpened():
+	
+	time_elapsed = time.time() - prev
+
+	if time_elapsed > 1./fps:
+		print(time_elapsed)
+
+		# Get a new frame
+		ret, frame = cap.read()
+
+		#print(type(frame))
+		
+		# No more frames
+		if not ret:
+			break
+		
+		prev = time.time()
+		cv2.imshow('frame_resized', rescale_frame(frame, 1280, 856))
+
+	
+	# the 'q' button is set as the
+	# quitting button you may use any
+	# desired button of your choice
+	if cv2.waitKey(1) & 0xFF == ord('q'):
+		break
+
+# After the loop release the cap object
+cap.release()
+# Destroy all the windows
+cv2.destroyAllWindows()
+
