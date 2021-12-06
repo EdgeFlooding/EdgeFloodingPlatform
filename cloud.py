47bb576c5fba67113cf3189de5d4e69329a4ffda
Andrea <andrea.klaus.tubak@gmail.com>
2021-12-06 18:21:50 +0100

    [TBT] Keeping track of time spent in retreiving last tweets

--- cloud.py
+++ cloud.py
@@ -13,6 +13,37 @@ import grpc_services_pb2
 import grpc_services_pb2_grpc
 
 
+def synchronized(lock):
+    """ Synchronization decorator. """
+
+    def wrap(f):
+        def newFunction(*args, **kw):
+            lock.acquire()
+            try:
+                return f(*args, **kw)
+            finally:
+                lock.release()
+        return newFunction
+    return wrap
+
+
+class Tweets():
+
+    lock = threading.Lock()
+    last_tweets = None
+    time_for_request = 0
+
+    @synchronized(lock)
+    def update_last_tweets(self, new_tweets, new_time_required):
+        self.last_tweets = new_tweets
+        self.time_for_request = new_time_required
+
+    @synchronized(lock)
+    def get_last_tweets(self):
+        return self.last_tweets, self.time_for_request
+
+
+
 def current_time_int():
     return int(round(time.time() * 1000_000_000))
 
@@ -63,7 +94,7 @@ def connect_to_endpoint(url, params):
     return response.json()
 
 
-def update_tweets(run_event, lock, last_tweets, n_seconds, logger):
+def update_tweets(run_event, tweets_obj, n_seconds, logger):
     '''Periodically queries twitter to get the last tweets of UMBC Flood Bot and update the last_tweets'''
     # UMBC Flood Bot id
     bot_id = 1173295284874596358
@@ -73,36 +104,37 @@ def update_tweets(run_event, lock, last_tweets, n_seconds, logger):
 
     while not run_event.is_set():
         try:
+            start_ts = current_time_int()
             json_response = connect_to_endpoint(url, params)
+            end_ts = current_time_int()
         except Exception:
             print("Cannot retrieve json")
             logger.info("[TWITTER_ERROR]")
             run_event.wait(n_seconds)
             continue
 
+        last_tweets, _ = tweets_obj.get_last_tweets()
         # Mutual exclusion with server threads
-        lock.acquire()
         # check if last_tweets must be updated or not
         if last_tweets is None or last_tweets['meta']['newest_id'] != json_response['meta']['newest_id']:
-            last_tweets = json_response
-        lock.release()
-
+            time_for_request = end_ts - start_ts
+            tweets_obj.update_last_tweets(json_response, time_for_request)
+        
         run_event.wait(n_seconds)
 
 
 # based on .proto service
 class AggregateResultServicer(grpc_services_pb2_grpc.ResultProcedureServicer):
 
-    def __init__(self, logger, lock, last_tweets):
+    def __init__(self, logger, tweets_obj):
         self.logger = logger
-        self.lock = lock
-        self.last_tweets = last_tweets
+        self.tweets_obj = tweets_obj
 
 
     def AggregateResult(self, request, context):
         start_ts = current_time_int()
         response = grpc_services_pb2.Empty()
-      
+
         id_node = request.id_node
         id_frame = request.id_frame
         id_camera = request.id_camera
@@ -121,11 +153,11 @@ class AggregateResultServicer(grpc_services_pb2_grpc.ResultProcedureServicer):
         the_day = "The day is"
         # If rain intensity is above this value it is a strong signal for possible flooding
         threshold = 0.5
-
-        self.lock.acquire()
         
+        last_tweets, time_for_tweets = self.tweets_obj.get_last_tweets()
+
         # The twitter_thread hasn't updated this value yet
-        if self.last_tweets is None:
+        if last_tweets is None:
             dict_to_save['Flooding'] = False
 
         else:
@@ -138,23 +170,21 @@ class AggregateResultServicer(grpc_services_pb2_grpc.ResultProcedureServicer):
                 rain_intensity_float = float(text[rain_index + len(rain_intensity):end_index])
                 dict_to_save['Flooding'] = rain_intensity_float > threshold 
 
-        self.lock.release()
-
         self.logger.info(f"[AGGREGATION]: {json.dumps(dict_to_save)}")
 
         end_ts = current_time_int()
-        self.logger.info(f'[AGG_LATENCY] {{"Time": {end_ts-start_ts}}}')
+        self.logger.info(f'[AGG_LATENCY] {{"Time": {end_ts - start_ts}, "Time_for_tweets": {time_for_tweets}}}')
         return response
 
 
-def start_server(logger, lock, last_tweets):
+def start_server(logger, tweets_obj):
     # create a gRPC server
     server = grpc.server(futures.ThreadPoolExecutor(max_workers=12))
 
 
     # add the defined class to the server
     grpc_services_pb2_grpc.add_ResultProcedureServicer_to_server(
-            AggregateResultServicer(logger, lock, last_tweets), server)
+            AggregateResultServicer(logger, tweets_obj), server)
 
     # listen on port 5000
     print('Starting server. Listening on port 5000.')
@@ -171,16 +201,16 @@ def main():
     log_name = sys.argv[1]
 
     # Hard coded because we have a limit on the number of request we can issue to the twitter endpoint
-    n_seconds = 300
-    last_tweets = None
-    lock = threading.Lock()
+    n_seconds = 20
+    tweets_obj = Tweets()
 
     logger = logger_setup(log_name)
-    server = start_server(logger, lock, last_tweets)
+    server = start_server(logger, tweets_obj)
 
     run_event = threading.Event()
-    twitter_thread = threading.Thread(target = update_tweets, args = (run_event, lock, last_tweets, n_seconds, logger))
+    twitter_thread = threading.Thread(target = update_tweets, args = (run_event, tweets_obj, n_seconds, logger))
     twitter_thread.start()
+
 
     try:
         while True:
