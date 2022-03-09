f4c5c9777a96e0fd8b3beb01d518a30aef992506
Andrea Tubak <andrea.klaus.tubak@gmail.com>
2022-03-09 18:52:41 +0100

    [TBT] camera with ack lag log

--- camera.py
+++ camera.py
@@ -9,6 +9,7 @@ import cv2
 import time
 import sys
 import base64
+import logging
 
 '''
 This is a single thread producer script emulating a camera
@@ -22,7 +23,7 @@ def current_time_int():
     return int(round(time.time() * 1000_000_000))
 
 
-def produce(stub, id_frame_slot, ip_consumer, channel, port):
+def produce(stub, id_frame_slot, ip_consumer, channel, port, logger):
     video_path = 'videos/Rec_20200125_170152_211_S.mp4'
 
     # Define a video capture object
@@ -35,7 +36,8 @@ def produce(stub, id_frame_slot, ip_consumer, channel, port):
     # To playback at the right framerate
     fps = cap.get(cv2.CAP_PROP_FPS)
     prev_update_timestamp = 0
-
+    frame_sent_timestamp = 0
+    ack_received_timestamp = 0
 
     print("Producing...", str(id_frame_slot)) # DEBUG
     print("Waiting for server...", end='', flush=True)
@@ -64,7 +66,6 @@ def produce(stub, id_frame_slot, ip_consumer, channel, port):
                 print("The frames of the video are finished -> Producer",str(id_frame_slot), "exiting")
                 break
 
-
             b64_frame = base64.b64encode(frame)
             frame_req = grpc_services_pb2.Frame(id=id_frame, id_slot=id_frame_slot, b64image=b64_frame, width=frame.shape[1],
              height=frame.shape[0], creation_timestamp=current_time_int())
@@ -72,6 +73,7 @@ def produce(stub, id_frame_slot, ip_consumer, channel, port):
             # make the call
             while True:
                 try:
+                    frame_sent_timestamp = current_time_int()
                     stub.HandleNewFrame(frame_req)
                 except:
                     print(".", end='', flush=True)
@@ -83,6 +85,11 @@ def produce(stub, id_frame_slot, ip_consumer, channel, port):
                     continue
                 break
 
+            ack_received_timestamp = current_time_int()
+            
+            if logger is not None:
+                logger.info(f'[ACK_LAG] {{"Frame_sent_timestamp": {frame_sent_timestamp}, "Frame_received_timestamp": {ack_received_timestamp}}}')
+
             prev_update_timestamp = time.time()
 
             print("Sent frame:", str(id_frame), "from producer", str(id_frame_slot)) # DEBUG
@@ -91,6 +98,17 @@ def produce(stub, id_frame_slot, ip_consumer, channel, port):
 
     # After the loop release the cap object
     cap.release()
+
+
+def logger_setup(log_file):
+    # Remove all handlers associated with the root logger object.
+    for handler in logging.root.handlers[:]:
+        logging.root.removeHandler(handler)
+
+    # logger configuration
+    LOG_FORMAT = "%(levelname)s %(asctime)s %(message)s"
+    logging.basicConfig(filename=log_file, level=logging.INFO, format=LOG_FORMAT, filemode="w")
+    return logging.getLogger()
 
 
 def check_int(int_str):
@@ -109,16 +127,22 @@ def main():
 
     # Check number of arguments
     n_arguments = len(sys.argv)
-    if n_arguments != 3:
-        exit("The number of argument is not correct\nPlease provide: ID frame slot and IP consumer node")
+    if n_arguments != 3 or n_arguments != 4:
+        exit("The number of argument is not correct\nPlease provide: ID frame slot, IP consumer node and log file name (optional)")
 
     # Check type of arguments
     if not check_int(sys.argv[1]):
         # Error prints are already in the check functions
         exit()
 
+    logger = None
+
+    # Read inputs
     id_frame_slot = int(sys.argv[1])
     ip_consumer = sys.argv[2]
+
+    if n_arguments == 4:
+        logger = logger_setup(sys.argv[3])
     port = 5000 + id_frame_slot
 
     print("All good, extracting frames...")
@@ -127,7 +151,7 @@ def main():
         channel = grpc.insecure_channel(f'{ip_consumer}:{port}')
         # create a stub (client)
         stub = grpc_services_pb2_grpc.FrameProcedureStub(channel)
-        produce(stub, id_frame_slot, ip_consumer, channel, port)
+        produce(stub, id_frame_slot, ip_consumer, channel, port, logger)
     except KeyboardInterrupt:
         exit("\nExiting...")
     except Exception as e:
